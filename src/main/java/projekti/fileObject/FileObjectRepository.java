11e8add81a0a000ac960b3d95cc8be0f3ac6520c
Mikko Pelkonen <m.a.pelkonen2@gmail.com>
2020-06-25 22:26:11 +0300

    Even more fine-tuning

--- src/main/java/projekti/fileObject/FileObjectRepository.java
+++ src/main/java/projekti/fileObject/FileObjectRepository.java
@@ -1,7 +1,9 @@
 package projekti.fileObject;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 public interface FileObjectRepository extends JpaRepository<FileObject, Long> {
     FileObject findByAccountId(Long id);
 }
